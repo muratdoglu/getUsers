28d9214cb40aa1ef5f665248316ab504e29ac028
Murat Değirmencioğlu <muratdoglu@gmail.com>
2023-09-17 15:36:51 +0300

    asfasf

--- functions/getUsers/src/main.py
+++ functions/getUsers/src/main.py
@@ -1,37 +1,142 @@
+import json
 from appwrite.client import Client
-import os
 
+# You can remove imports of services you don't use
+from appwrite.services.account import Account
+from appwrite.services.avatars import Avatars
+from appwrite.services.databases import Databases
+from appwrite.services.functions import Functions
+from appwrite.services.health import Health
+from appwrite.services.locale import Locale
+from appwrite.services.storage import Storage
+from appwrite.services.teams import Teams
+from appwrite.services.users import Users
+from appwrite.query import Query
+from datetime import datetime, timedelta
+"""
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
 
-# This is your Appwrite function
-# It's executed each time we get a request
-def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
+  'res' variable has:
+    'send(text, status)' - function to return text response. Status code defaults to 200
+    'json(obj, status)' - function to return JSON response. Status code defaults to 200
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
+  If an error is thrown, a response with code 500 will be returned.
+"""
 
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
+def main(req, res):
+  client = Client()
+  client.set_endpoint("http://162.19.255.235/v1")
+  client.set_project("6506073c9158e012f6d3")
+  client.set_key("f70c6d75b3cd8878c999dd211a18509d52269c25d4bb5f4fc8e8edc42fec27e7087b29e51ec6fbb52546f520757aa46b3624902f5b58cfc8a0fc1d3b30b5cd20602f6b6e156603551c40b97d99534d841bb1e175093f954d7cc9d3fb41a78e75bc60740d67b4b69ef911392029892d6a8ae0698cc981df34d100b2fccc45aa5b")
+  client.set_self_signed(True)
+    
+    
+  # You can remove services you don't use
+  account = Account(client)
+  avatars = Avatars(client)
+  database = Databases(client)
+  functions = Functions(client)
+  health = Health(client)
+  locale = Locale(client)
+  storage = Storage(client)
+  teams = Teams(client)
+  users = Users(client)
+  payload = json.loads(req.payload)
+  gender = payload['gender']
+ 
+    # online olanlar
 
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
+  allUsers = []
 
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build Fast. Scale Big. All in One Place.",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+  print(1)
+  print("123")
+  allUsers.clear()
+    # hiç aranmamışlar
+    
+     # son 30 dakikada online olanlar
+  current_time2= datetime.now()
+  two_days_ago2 = current_time2 - timedelta(2)
+  filters52 = [Query.greater_than('lastOnline', two_days_ago2),
+                Query.equal('gender', gender),
+                  Query.equal('isOnline', True),
+                   Query.equal('live', True)
+                ]
+  data52 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters52)
+  documents52 = data52['documents']
+  allUsers += documents52 # Append documents2 to allUsers
+  
+    # son 30 dakikada online olanlar
+  current_time1= datetime.now()
+  two_days_ago1 = current_time1 - timedelta(2)
+  filters51 = [Query.greater_than('lastOnline', two_days_ago1),
+                Query.equal('gender', gender),
+                  Query.equal('isOnline', True)
+                ]
+  data51 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters51)
+  documents51 = data51['documents']
+  allUsers += documents51 # Append documents2 to allUsers
+  
+  
+  filters2 = [Query.equal('callGuestCount', 0),
+                Query.equal('gender', gender),
+                 Query.equal('isOnline', True)]
+  data2 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters2)
+  documents2 = data2['documents']
+
+  allUsers += documents2  # Append documents2 to allUsers
+  print(allUsers)
+
+    #  arama alma ve cevaplama oranı +5 olanlar
+  filters3 = [Query.greater_than('callGuestAnsweredPoint', 5),
+                Query.equal('gender', gender),
+                  Query.equal('isOnline', True)]
+  data3 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters3)
+  documents3 = data3['documents']
+
+  allUsers += documents3  # Append documents2 to allUsers
+  print(allUsers)
+
+    #  aktif arama yapanlar
+  filters4 = [Query.greater_than('callHostCount', 40),
+                Query.greater_than('callGuestAnsweredPoint', -5),
+                Query.equal('gender', gender),
+                  Query.equal('isOnline', True)]
+  data4 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters4)
+  documents4 = data4['documents']
+  allUsers += documents4  # Append documents2 to allUsers
+
+  print(allUsers)
+
+    # son 2 günde online olanlar
+  current_time = datetime.now()
+  two_days_ago = current_time - timedelta(2)
+  filters5 = [Query.greater_than('lastOnline', two_days_ago),
+                Query.equal('gender', gender),
+                  Query.equal('isOnline', True)
+                ]
+  data5 = database.list_documents('6506087581ce0a10ef2a', "6506088f22805d12b4fe", filters5)
+  documents5 = data5['documents']
+  allUsers += documents5  # Append documents2 to allUserssource myenv/bin/activate
+
+  
+  print("kemal")
+  for user in allUsers:
+    del user["$collectionId"]
+    del user["$createdAt"]
+    del user["$databaseId"]
+    del user["$id"]
+    del user["$permissions"]
+    del user["$updatedAt"]
+    del user["createdDate"]
+    del user["lastOnline"]
+
+  print(allUsers)
+  
+  return res.json({
+    "users": allUsers,
+  })
+  
+  
